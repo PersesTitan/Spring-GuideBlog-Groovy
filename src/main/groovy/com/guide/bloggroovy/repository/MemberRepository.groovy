5996e13685836144d94c116bcb159403a129f89d
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-12 17:21:52 +0900

    create MemberRepository

---
+++ src/main/groovy/com/guide/bloggroovy/repository/MemberRepository.groovy
@@ -0,0 +1,27 @@
+package com.guide.bloggroovy.repository
+
+import com.guide.bloggroovy.domain.Member
+import lombok.RequiredArgsConstructor
+import org.springframework.stereotype.Repository
+
+import javax.persistence.EntityManager
+
+@Repository
+@RequiredArgsConstructor
+class MemberRepository {
+
+    private final EntityManager em;
+
+    void save(Member member) {
+        em.persist(member)
+    }
+
+    Member findOne(Long id) {
+        return em.find(Member.class, id)
+    }
+
+    List<Member> findAll() {
+        return em.createQuery("SELECT Member FROM Member", Member.class)
+                .getResultList()
+    }
+}
