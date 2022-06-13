4a3c1023d8a4416703836a6d1a652dbb7a04aa09
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-13 20:21:37 +0900

    Set Test

--- src/test/groovy/com/guide/bloggroovy/BlogGroovyApplicationTests.groovy
+++ src/test/groovy/com/guide/bloggroovy/BlogGroovyApplicationTests.groovy
@@ -1,5 +1,9 @@
 package com.guide.bloggroovy
 
+import lombok.AllArgsConstructor
+import lombok.Getter
+import lombok.RequiredArgsConstructor
+import lombok.Setter
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
@@ -8,6 +12,34 @@ class BlogGroovyApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Mem mem = new Mem("A", "b")
+		println mem.getName()
 	}
 
+	@Setter @Getter
+	class Mem {
+		private String name
+		private String date
+
+		String getName() {
+			return name
+		}
+
+		void setName(String name) {
+			this.name = name
+		}
+
+		String getDate() {
+			return date
+		}
+
+		void setDate(String date) {
+			this.date = date
+		}
+
+		Mem(String name, String date) {
+			this.name = name
+			this.date = date
+		}
+	}
 }
