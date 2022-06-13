539bddc4aa65128c7cf50cb99999bb635b6a71f2
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-13 15:08:20 +0900

    add BoarderRepository

--- src/main/groovy/com/guide/bloggroovy/service/BoardService.groovy
+++ src/main/groovy/com/guide/bloggroovy/service/BoardService.groovy
@@ -1,5 +1,15 @@
 package com.guide.bloggroovy.service
 
+import com.guide.bloggroovy.repository.BoardRepository
+import lombok.RequiredArgsConstructor
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 class BoardService {
+
+    private final BoardRepository boardRepository
 
 }
