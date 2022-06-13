549776035579f9e870a36d26359d5870841ee6d8
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-13 20:22:42 +0900

    생성자 추가

--- src/main/groovy/com/guide/bloggroovy/repository/BoardRepository.groovy
+++ src/main/groovy/com/guide/bloggroovy/repository/BoardRepository.groovy
@@ -1,14 +1,19 @@
 package com.guide.bloggroovy.repository
 
 import lombok.RequiredArgsConstructor
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Repository
 
 import javax.persistence.EntityManager
 
 @Repository
-@RequiredArgsConstructor
 class BoardRepository {
 
     private final EntityManager em
+
+    @Autowired
+    BoardRepository(EntityManager em) {
+        this.em = em
+    }
 
 }
