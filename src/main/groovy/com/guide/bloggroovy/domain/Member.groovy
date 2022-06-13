ab8410b3e896d0cf9f2f24b69f2522a59ee79a90
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-13 20:25:05 +0900

    Setter, Getter 추가

--- src/main/groovy/com/guide/bloggroovy/domain/Member.groovy
+++ src/main/groovy/com/guide/bloggroovy/domain/Member.groovy
@@ -12,29 +12,67 @@ import javax.persistence.GeneratedValue
 import javax.persistence.Id
 import java.time.LocalDateTime
 
-@Entity @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
 class Member {
 
     @Id @GeneratedValue
     @Column(name = "member_id")
     private Long id
 
-    @Setter @NonNull
-    private String nickname
-    @Setter @NonNull
-    private String password
+    @NonNull private String nickname
+    @NonNull private String password
     @NonNull private String loginId
     @NonNull private LocalDateTime createDate
 
     @Setter private String email
 
+    //생성자
     private Member(String nickname, String password, String loginId, String email) {
         this.nickname = nickname
         this.password = password
         this.loginId = loginId
         this.email = email
         this.createDate = LocalDateTime.now()
+    }
+
+    protected Member() {}
+
+    //Setter
+    void setNickname(String nickname) {
+        this.nickname = nickname
+    }
+
+    void setPassword(String password) {
+        this.password = password
+    }
+
+    void setEmail(String email) {
+        this.email = email
+    }
+
+    //Getter
+    Long getId() {
+        return id
+    }
+
+    String getNickname() {
+        return nickname
+    }
+
+    String getPassword() {
+        return password
+    }
+
+    String getLoginId() {
+        return loginId
+    }
+
+    LocalDateTime getCreateDate() {
+        return createDate
+    }
+
+    String getEmail() {
+        return email
     }
 
     //생성 메소드
