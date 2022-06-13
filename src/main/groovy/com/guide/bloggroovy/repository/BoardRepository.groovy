3cfd0d3a2e1528459d0e7bd9470eefdab2f2d065
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-13 15:08:02 +0900

    add EntityManager

--- src/main/groovy/com/guide/bloggroovy/repository/BoardRepository.groovy
+++ src/main/groovy/com/guide/bloggroovy/repository/BoardRepository.groovy
@@ -1,8 +1,14 @@
 package com.guide.bloggroovy.repository
 
+import lombok.RequiredArgsConstructor
 import org.springframework.stereotype.Repository
 
+import javax.persistence.EntityManager
+
 @Repository
+@RequiredArgsConstructor
 class BoardRepository {
+
+    private final EntityManager em
 
 }
