3d2db17296a1b135bab790d142ba6780ba4857d9
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-13 20:22:19 +0900

    Set Setter, Getter

--- src/main/groovy/com/guide/bloggroovy/domain/Board.groovy
+++ src/main/groovy/com/guide/bloggroovy/domain/Board.groovy
@@ -16,17 +16,15 @@ import java.time.LocalDateTime
 
 import static javax.persistence.FetchType.*
 
-@Entity @Getter
+@Entity
 class Board {
 
     @Id @GeneratedValue
     @Column(name = "board_id")
     private Long id
 
-    @NonNull @Setter
-    private String title
-    @NonNull @Setter
-    private String content
+    @NonNull private String title
+    @NonNull private String content
 
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "member_id")
@@ -34,6 +32,7 @@ class Board {
 
     private LocalDateTime createDate
 
+    //생성자
     private Board(String title, String content, Member member) {
         this.title = title
         this.content = content
@@ -42,6 +41,36 @@ class Board {
     }
 
     protected Board() {}
+
+    //Setter
+    void setTitle(String title) {
+        this.title = title
+    }
+
+    void setContent(String content) {
+        this.content = content
+    }
+
+    //Getter
+    Long getId() {
+        return id
+    }
+
+    String getTitle() {
+        return title
+    }
+
+    String getContent() {
+        return content
+    }
+
+    Member getMember() {
+        return member
+    }
+
+    LocalDateTime getCreateDate() {
+        return createDate
+    }
 
     //생성 메소드
     static createBoard(Member member, String title, String content) {
