c8214fe5b99c21ab615c2d93aeed16a7191c683b
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-14 10:38:55 +0900

    필요없는 로직 삭제

--- src/main/groovy/com/guide/bloggroovy/domain/Board.groovy
+++ src/main/groovy/com/guide/bloggroovy/domain/Board.groovy
@@ -1,10 +1,6 @@
 package com.guide.bloggroovy.domain
 
-import lombok.AccessLevel
-import lombok.Getter
-import lombok.NoArgsConstructor
 import lombok.NonNull
-import lombok.Setter
 
 import javax.persistence.Column
 import javax.persistence.Entity
